63e7ed4e2a87ce6485f7b791d370da041b51bbec
Emmanuell87 <emmanuelmejia2003@gmail.com>
2022-08-07 01:39:30 -0500

    configuracion express-validator

--- server/src/routes/users.routes.ts
+++ server/src/routes/users.routes.ts
@@ -1,7 +1,11 @@
 import { Router } from "express";
 import { signIn, signUp } from "../controllers/users.controller";
+import {
+	checkSchema_UsersSignIn,
+	checkSchema_UsersSignUp,
+} from "../middlewares/validator.middleware";
 
 const router = Router();
 
-router.post("/signUp", signUp);
-router.post("/signIn", signIn);
+router.post("/signUp", checkSchema_UsersSignUp, signUp);
+router.post("/signIn", checkSchema_UsersSignIn, signIn);
