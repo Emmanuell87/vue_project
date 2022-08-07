ab1f0fa789119c558390fb7c4418503dcd2c23e2
Emmanuell87 <emmanuelmejia2003@gmail.com>
2022-08-06 22:29:11 -0500

    creacion server y organizacion en diferentes carpetas

---
+++ server/src/app.ts
@@ -0,0 +1,30 @@
+import express, { Application } from "express";
+import cors from "cors";
+import morgan from "morgan";
+
+//routes
+
+export class App {
+	private app: Application;
+
+	constructor(private port?: number | string) {
+		this.app = express();
+		this.settings();
+		this.middlewares();
+	}
+
+	middlewares(): void {
+		this.app.use(cors());
+		this.app.use(morgan("dev"));
+	}
+
+	settings(): void {
+		this.app.set("port", process.env.PORT || this.port || 3000);
+	}
+
+	listen(): void {
+		this.app.listen(this.app.get("port"), () => {
+			console.log(`Listening on port ${this.app.get("port")}`);
+		});
+	}
+}
