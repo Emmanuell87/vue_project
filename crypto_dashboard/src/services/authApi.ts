c9855c0521efaade0b1f137f75c356480fedf50b
Emmanuell87 <emmanuelmejia2003@gmail.com>
2022-08-07 09:43:10 -0500

    auth api

---
+++ crypto_dashboard/src/services/authApi.ts
@@ -0,0 +1,38 @@
+import API from "./axiosConfig";
+
+import { AxiosError, AxiosResponse } from "axios";
+import { IUser, IMessageApi } from "@/interface/api.interface";
+
+const config = {
+	headers: {
+		enctype: "application/x-www-form-urlencoded",
+	},
+};
+
+export const signInApi = async (user: IUser): Promise<string | IMessageApi> => {
+	return await API.post("/signIn", user, config)
+		.then((response: AxiosResponse) => {
+			return response.data.token;
+		})
+		.catch((error: AxiosError) => {
+			if (error.response?.data) {
+				return error.response.data as IMessageApi;
+			} else {
+				return { message: "An internal error occurred" };
+			}
+		});
+};
+
+export const signUpApi = async (
+	user: IUser
+): Promise<boolean | IMessageApi> => {
+	return await API.post("/signUp", user)
+		.then(() => true)
+		.catch((error: AxiosError) => {
+			if (error.response?.data) {
+				return error.response.data as IMessageApi;
+			} else {
+				return { message: "An internal error occurred" };
+			}
+		});
+};
