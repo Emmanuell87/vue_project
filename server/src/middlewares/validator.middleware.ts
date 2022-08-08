ea64e72fff219fc78ced155d5c51f2b4d0f7d0bf
Emmanuell87 <emmanuelmejia2003@gmail.com>
2022-08-08 18:03:24 -0500

    validar errors express-validator actualizado

--- server/src/middlewares/validator.middleware.ts
+++ server/src/middlewares/validator.middleware.ts
@@ -15,7 +15,7 @@ export const validate = (validations: ValidationChain[]) => {
 	return async (req: Request, res: Response, next: NextFunction) => {
 		for (let validation of validations) {
 			const result = await validation.run(req);
-			if (result.errors.length) break;
+			if (result.isEmpty()) break;
 		}
 
 		const errorFormatter = ({
